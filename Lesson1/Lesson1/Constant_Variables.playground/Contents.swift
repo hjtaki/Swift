490597cbb9aa7a1d1047f529b844486c484ae2a4
hjtaki <hjtaki@gmail.com>
2018-09-25 10:41:46 -0700

    lessons

---
+++ Lesson1/Lesson1/Constant_Variables.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+let messege = "You can do it !"
+//messege = 12
+
+
+// String interpolation
+var name : String = "Alex"
+var messageWithName = "Youcan do it, \(name)"
+
+messageWithName.replacingOccurrences(of: "Alex", with: "Han")
+
+let year = [1990, 1994, 1998, 2002]
+
+
+
+
+
---
+++ Lesson1/Lesson1/L1_Exercises.playground/Contents.swift
@@ -0,0 +1,135 @@
+//: # Lesson 1 Exercises
+//: ## String Manipulation
+import UIKit
+import Foundation
+//: ### Exercise 1
+//: Example: Here I've declared one String that forms a sentence that makes sense. I've declared a second String that forms a silly sentence when random words are chosen.
+let nounArray = ["puppy", "laptop", "ocean","app", "cow", "skateboard", "developer", "coffee", "moon"]
+
+let index1 = Int(arc4random_uniform(9))
+let index2 = Int(arc4random() % 9)
+
+let sentence = "The \(nounArray[6]) spilled her \(nounArray[7])."
+let sillySentence = "The \(nounArray[index1]) jumped over the \(nounArray[index2])."
+//: Now try it yourself! Declare a new string that incorporates objects from the noun array above. Write one sentence that makes sense and one "Madlib" sentence with randomly chosen words. Feel free to add words to the noun array or declare a new array.
+let yourSentence = "The \(nounArray[6]) liked the \(nounArray[7])."
+let yourSillySentence = "The \(nounArray[index1]) break the \(nounArray[index2])."
+//: ### Exercise 2
+//: Recreate the shoutString by using the didYouKnowString as a stem.
+let didYouKnowString = "Did you know that the Swift String class comes with lots of useful methods?"
+let whisperString = "psst" + ", " + didYouKnowString.lowercased()
+let shoutString =  "HEY! \(didYouKnowString.uppercased())"
+//: ### Exercise 3
+//: How many characters are in this string? 
+let howManyCharacters = "How much wood could a woodchuck chuck if a woodchuck could chuck wood?"
+
+howManyCharacters.count
+//: ### Exercise 4
+//: How many times does the letter "g" or "G" appear in the following string? Use a for-in loop to find out!
+let gString = "Gary's giraffe gobbled gooseberries greedily"
+let a = gString
+a
+var count = 0
+for ch in gString{
+    print(ch)
+    if(ch == "g" || ch == "G"){
+        count += 1
+    }
+}
+count
+
+
+
+//: ### Exercise 5
+//: Write a program that tells you whether or not this string contains the substring "tuna".
+let word = "fortunate"
+word.contains("tuna")
+
+//: ### Exercise 6
+//: Write a program that deletes all occurrences of the word "like" in the following string.
+let lottaLikes = "If like, you wanna learn Swift, like, you should build lots of small apps, cuz it's like, a good way to practice."
+
+lottaLikes.replacingOccurrences(of: "like", with: "")
+
+
+//: ### Exercise 7
+// Example
+let sillyMonkeyString = "A monkey stole my iPhone"
+let newString = sillyMonkeyString.replacingOccurrences(of: "monkey", with: "🐒")
+let newerString = newString.replacingOccurrences(of: "iPhone", with: "📱")
+
+//: Repeat the above string manipulation, but this time using a for-in loop.
+//: You can start off with this dictionary and string.
+let dictionary = ["monkey": "🐒", "iPhone":"📱"]
+var output: String = ""
+var newestString = sillyMonkeyString
+
+for word in newestString.split(separator:" "){
+    if let val = dictionary[String(word)]{
+        output += val + " "
+    }else{
+        output += word + " "
+    }
+}
+
+print(output)
+
+
+//: ### Exercise 8
+//: Josie has been saving her pennies and has them all counted up. Write a program that, given a number of pennies, prints out how much money Josie has in dollars and cents.
+// Example
+let numOfPennies = 567
+let dollors = "$\(numOfPennies/100).\(numOfPennies % 100)"
+print(dollors)
+
+//desired output = "$5.67"
+//: # Let or Var?
+import UIKit
+import Foundation
+//: ### Exercise 9
+//: Below is the code to find all the numbers present in an array, convert them to Ints, and calculate their sum. Have a look at the entities declared below: array, sum, and intToAdd. Think about whether each should be a constant or a variable and choose whether to declare them with let or var. When you're finished uncomment the code and see if the compiler agrees with your choices!
+ let array = ["A", "13", "B", "5","87", "t", "41"] // TODO: Choose constant or variable
+ var sum = 0 // TODO: Choose constant or variable
+ for string in array {
+    
+    
+     if Int(string) != nil {
+         let intToAdd = Int(string)! // TODO: Choose constant or variable
+         sum += intToAdd
+     }
+    
+//    or
+//    if let intToadd = Int(string){
+//        sum += intToadd
+//    }
+    
+ }
+
+
+
+//: ### Exercise 10
+//: For each of the following pairs, choose whether to declare a constant or a variable.
+//:
+//: Example: Two hikers are climbing up to the summit of a mountain. Along the way, they stop a few times to check their current elevation.
+//:
+//: Example response:
+let summitElevation: Int
+var currentElevation: Int
+//: 10a) Imagine you are writing a quiz app, and you need to program a timer that will stop a quiz after 20 min. Declare four entities: startTime, currentTime, maximumTimeAllowed, and timeRemaining. Don't worry about encoding their values.
+let startTime : Double
+var currentTime : Double
+let maximumTimeAllowed: Double
+var timeRemaining: Double
+
+
+//: 10b) Imagine you are writing an app for a credit card company. Declare two entities: creditLimit and balance.
+let creditLimit: Double
+var balance: Double
+//: ### Exercise 11
+//: Below is the code to reverse a string. Have a look at the entities declared: stringToReverse and reversedString. Choose whether to declare each with let or var. When you're finished uncomment the code and see if the compiler agrees with your choices!
+let stringToReverse = "Mutable or Immutable? That is the question." // TODO:Choose let or var
+var reversedString = "" //TODO:Choose let or var
+for character in stringToReverse {
+    reversedString = "\(character)" + reversedString
+}
+print(reversedString, terminator: "")
